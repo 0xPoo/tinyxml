d445077b7c5812f3907a4f64fca9b25dc0dc641f
0xPoo <spermbankdao@protonmail.com>
2021-12-22 22:45:11 +1300

    feat: add all files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tiny XML Parser
+Wrote this for fun, use as you will.
+
+### Compiling
+I haven't included a Makefile because the codebase is tiny and I see no need.
+
+To build the test file, simply run: `gcc test.c -o test`
+
+To run the test executable use: `./test`
---
+++ test.c
@@ -0,0 +1,21 @@
+#include "txml.h"
+
+int main()
+{
+    XMLDocument doc;
+    if (XMLDocument_load(&doc, "test.xml")) {
+        XMLNode* str = XMLNode_child(doc.root, 0);
+
+        XMLNodeList* fields = XMLNode_children(str, "field");
+        for (int i = 0; i < fields->size; i++) {
+            XMLNode* field = XMLNodeList_at(fields, i);
+            XMLAttribute* type = XMLNode_attr(field, "type");
+            type->value = "";
+        }
+
+        XMLDocument_write(&doc, "out.xml", 4);
+        XMLDocument_free(&doc);
+    }
+
+    return 0;
+}
